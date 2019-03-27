e7ba45869639d0e46c69e24ae2ca34a7bdf1dc3b
Rok Gregorič <rok.gregoric@gmail.com>
2019-03-27 10:06:49 +0100

    Added UIGestureRecognizer.State.name.

---
+++ Extensions/UIGestureRecognizerExtensions.swift
@@ -0,0 +1,21 @@
+//
+//  UIGestureRecognizerExtensions.swift
+//
+//  Created by Rok Gregorič
+//  Copyright © 2018 Rok Gregorič. All rights reserved.
+//
+
+import UIKit
+
+extension UIGestureRecognizer.State {
+  var name: String {
+    switch self {
+      case .possible: return "possible"
+      case .began: return "began"
+      case .changed: return "changed"
+      case .ended: return "ended"
+      case .cancelled: return "cancelled"
+      case .failed: return "failed"
+    }
+  }
+}
