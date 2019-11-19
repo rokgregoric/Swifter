b0b82becff29cef83b0c8c9ffe6e3c861d080154
Rok Gregorič <rok.gregoric@gmail.com>
2019-11-19 11:55:15 +0100

    Updated UIDevice.sysVersion.

--- Extensions/UIDeviceExtensions.swift
+++ Extensions/UIDeviceExtensions.swift
@@ -9,6 +9,10 @@ import UIKit
 
 extension UIDevice {
   var sysVersion: Float {
-    return Float(systemVersion) ?? 0
+    let arr = systemVersion.components(separatedBy: ".")
+    let major = arr.object(at: 0).flatMap(Int.init) ?? 0
+    let minor = arr.object(at: 1).flatMap(Int.init) ?? 0
+    let str = String(format: "%d.%d", major, minor)
+    return Float(str) ?? 0
   }
 }
