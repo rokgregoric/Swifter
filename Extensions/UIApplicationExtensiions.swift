eebb3b0095c2eb3f24eb31a9c7626600e04f60f8
Rok Gregorič <rok.gregoric@gmail.com>
2019-12-16 05:59:20 +0100

    Added UIApplication.clearLaunchScreenCache.

---
+++ Extensions/UIApplicationExtensiions.swift
@@ -0,0 +1,19 @@
+//
+//  UIApplicationExtensiions.swift
+//
+//  Created by Rok Gregoric on 16/12/2019.
+//  Copyright © 2019 Rok Gregoric. All rights reserved.
+//
+
+import UIKit
+
+extension UIApplication {
+  // https://rambo.codes/ios/quick-tip/2019/12/09/clearing-your-apps-launch-screen-cache-on-ios.html
+  func clearLaunchScreenCache() {
+    do {
+      try FileManager.default.removeItem(atPath: NSHomeDirectory()+"/Library/SplashBoard")
+    } catch {
+      print("Failed to delete launch screen cache: \(error)")
+    }
+  }
+}
