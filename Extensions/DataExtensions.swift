c297304c96be25cba5096490e2dc30426052bdea
Rok Gregorič <rok.gregoric@gmail.com>
2020-11-12 06:53:21 +0100

    Improved Data(hexString:) performance.

--- Extensions/DataExtensions.swift
+++ Extensions/DataExtensions.swift
@@ -20,14 +20,16 @@ extension Data {
   init?(hexString: String) {
     let len = hexString.count / 2
     var data = Data(capacity: len)
-    for i in 0..<len {
-      let j = i*2
-      let bytes = hexString[j..<j+2]
+    var i = hexString.startIndex
+    for _ in 0..<len {
+      let j = hexString.index(i, offsetBy: 2)
+      let bytes = hexString[i..<j]
       if var num = UInt8(bytes, radix: 16) {
         data.append(&num, count: 1)
       } else {
         return nil
       }
+      i = j
     }
     self = data
   }
