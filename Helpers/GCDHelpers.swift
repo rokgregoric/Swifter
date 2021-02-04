7e07f92fedc8348c9ed840a2bd4ac4b671b59a36
Rok Gregorič <rok.gregoric@gmail.com>
2021-02-04 10:10:50 +0100

    Added Run.main(min:, block:, done:) - run after `min` delay or when `block` finishes.

--- Helpers/GCDHelpers.swift
+++ Helpers/GCDHelpers.swift
@@ -27,6 +27,14 @@ class Run {
   static func concurrent(after: Double = 0, _ block: @escaping () -> Void) {
     on(.global(qos: .userInteractive), after: after, block)
   }
+
+  static func main(min: Double, _ block: @escaping (@escaping () -> Void) -> Void, _ done: @escaping () -> Void) {
+    let g = DispatchGroup()
+    2.times(g.enter)
+    g.notify(queue: .main, execute: done)
+    main(after: min, g.leave)
+    block(g.leave)
+  }
 }
 
 
