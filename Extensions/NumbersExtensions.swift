cdb004c90ad84e80d8e3c4109ef2b4e66be63439
Rok Gregorič <rok.gregoric@gmail.com>
2022-05-25 06:09:17 +0200

    Added FloatingPoint.safe.

--- Extensions/NumbersExtensions.swift
+++ Extensions/NumbersExtensions.swift
@@ -113,4 +113,5 @@ extension BinaryInteger {
 
 extension FloatingPoint {
   func formatted(to decimals: Int) -> String { String(format: "%.\(decimals)f", self as! CVarArg) }
+  var safe: Self { isNaN ? 0 : self }
 }
