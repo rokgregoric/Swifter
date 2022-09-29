0aa970dc8641bb63ab50fd7d9360270dee999b52
Rok Gregorič <rok.gregoric@gmail.com>
2022-09-29 05:09:42 +0200

    Added Date.startOfDay.

--- Extensions/DateExtensions.swift
+++ Extensions/DateExtensions.swift
@@ -16,33 +16,21 @@ fileprivate var componentsFormatter: DateComponentsFormatter = {
 }()
 
 extension Date {
-  func adding(days: Int) -> Date {
-    return adding(days: Double(days))
-  }
+  func adding(days: Int) -> Date { adding(days: Double(days)) }
 
-  func adding(days: Double) -> Date {
-    return addingTimeInterval(3600*24*days)
-  }
+  func adding(days: Double) -> Date { addingTimeInterval(3600*24*days) }
 
-  var isToday: Bool {
-    return Calendar.current.isDateInToday(self)
-  }
+  var isToday: Bool { Calendar.current.isDateInToday(self) }
 
-  var isYesterday: Bool {
-    return Calendar.current.isDateInYesterday(self)
-  }
+  var isYesterday: Bool { Calendar.current.isDateInYesterday(self) }
 
-  var isTomorrow: Bool {
-    return Calendar.current.isDateInTomorrow(self)
-  }
+  var isTomorrow: Bool { Calendar.current.isDateInTomorrow(self) }
 
-  var isWeekend: Bool {
-    return Calendar.current.isDateInWeekend(self)
-  }
+  var isWeekend: Bool { Calendar.current.isDateInWeekend(self) }
 
-  var isInLast7days: Bool {
-    return Calendar.current.startOfDay(for: Date().adding(days: -7)) < self
-  }
+  var startOfDay: Date { Calendar.current.startOfDay(for: self) }
+
+  var isInLast7days: Bool { Date().adding(days: -7).startOfDay < self }
 
   var relativeString: String? {
     if self < Date() { return componentsFormatter.string(from: self, to: Date()) }
@@ -50,15 +38,13 @@ extension Date {
   }
 
   var longRelativeString: String? {
-    return relativeString.map { self < Date() ? "\($0) ago" : "in \($0)" }?.localized
+    relativeString.map { self < Date() ? "\($0) ago" : "in \($0)" }?.localized
   }
 
   @available(iOS 10.0, *)
   func iso8601string(timeZone: TimeZone = .UTC) -> String {
-    return ISO8601DateFormatter.string(from: self, timeZone: timeZone, formatOptions: .withInternetDateTime)
+    ISO8601DateFormatter.string(from: self, timeZone: timeZone, formatOptions: .withInternetDateTime)
   }
 
-  var timeIntervalTillNow: TimeInterval {
-    return -timeIntervalSinceNow
-  }
+  var timeIntervalTillNow: TimeInterval { -timeIntervalSinceNow }
 }
