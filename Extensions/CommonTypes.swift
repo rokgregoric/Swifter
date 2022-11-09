ce8c9784c88e223244f7b8417985dce2b16744ea
Rok Gregorič <rok.gregoric@gmail.com>
2022-11-09 04:55:44 +0100

    Added more CommonTypes.

--- Extensions/CommonTypes.swift
+++ Extensions/CommonTypes.swift
@@ -15,10 +15,18 @@ typealias EdgeInsets = UIEdgeInsets
 typealias Application = UIApplication
 typealias LayoutConstraint = NSLayoutConstraint
 typealias LayoutPriority = UILayoutPriority
+typealias Button = UIButton
+typealias CollectionView = UICollectionView
 
 var uniqueDeviceID: String? { UIDevice.current.identifierForVendor?.uuidString }
 var systemVersion: String { "\(UIDevice.current.systemName) \(UIDevice.current.systemVersion)" }
 let deviceName = UIDevice.current.name
+
+extension Application {
+  var isActive: Bool { applicationState == .active }
+  var isSleeping: Bool { applicationState == .background }
+  static let backgroundTaskInvalid = UIBackgroundTaskIdentifier.invalid
+}
 
 #elseif os(OSX)
 
@@ -30,10 +38,20 @@ typealias EdgeInsets = NSEdgeInsets
 typealias Application = NSApplication
 typealias LayoutConstraint = NSLayoutConstraint
 typealias LayoutPriority = NSLayoutConstraint.Priority
+typealias Button = NSButton
+typealias CollectionView = NSCollectionView
 
 var uniqueDeviceID: String? { nil }
 var systemVersion: String { "macOS \(NSAppKitVersion.current.rawValue)" }
 let deviceName = Host.current().localizedName ?? ""
+var sleepNotificationTriggered = false
+
+extension Application {
+  var isSleeping: Bool { sleepNotificationTriggered } // listen to NSWorkspace.willSleepNotification / .didWakeNotification
+  func endBackgroundTask(_ id: Int) {}
+  static let backgroundTaskInvalid = 0
+  func beginBackgroundTask(_: @escaping () -> Void) -> Int { 0 }
+}
 
 #endif
 
