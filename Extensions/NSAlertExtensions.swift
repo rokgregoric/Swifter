e58d6fe779eb0855aebbc915797a72f641cc49e9
Rok Gregorič <rok.gregoric@gmail.com>
2023-04-25 19:08:56 +0200

    NSAlert optional window.

--- Extensions/NSAlertExtensions.swift
+++ Extensions/NSAlertExtensions.swift
@@ -8,18 +8,18 @@
 import AppKit
 
 extension NSAlert {
-  static func show(title: String, message: String, accept: String = "Ok", on window: NSWindow, completion: @escaping () -> Void) {
+  static func show(title: String, message: String, accept: String = "Ok", on window: NSWindow? = nil, completion: @escaping () -> Void) {
     let a = NSAlert()
     a.messageText = title
     a.informativeText = message
     a.addButton(withTitle: accept)
     a.alertStyle = .warning
-    a.beginSheetModal(for: window) { _ in
+    a.present(on: window) { _ in
       completion()
     }
   }
 
-  static func ask(title: String, message: String, accept: String = "Yes", decline: String = "No", on window: NSWindow, destructive: Bool = false, completion: @escaping (Bool) -> Void) {
+  static func ask(title: String, message: String, accept: String = "Yes", decline: String = "No", on window: NSWindow? = nil, destructive: Bool = false, completion: @escaping (Bool) -> Void) {
     let a = NSAlert()
     a.messageText = title
     a.informativeText = message
@@ -31,12 +31,22 @@ extension NSAlert {
       a.addButton(withTitle: decline)
     }
     a.alertStyle = .warning
-    a.beginSheetModal(for: window) {
+    a.present(on: window) {
       if destructive {
         completion($0 == .alertSecondButtonReturn)
       } else {
         completion($0 == .alertFirstButtonReturn)
       }
     }
+  }
+
+  func present(on window: NSWindow? = nil, completion: @escaping (NSApplication.ModalResponse) -> Void) {
+    var window: NSWindow! = window
+    if window.isNil {
+      window = NSWindow(contentRect: .zero, styleMask: .borderless, backing: .buffered, defer: false)
+      window.center()
+    }
+    beginSheetModal(for: window, completionHandler: completion)
+    NSApp.activate(ignoringOtherApps: true)
   }
 }
