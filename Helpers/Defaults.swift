1d69c22804c89744fa4b6e6f96dde3014f62854d
Rok Gregorič <rok.gregoric@gmail.com>
2023-11-21 16:07:59 +0100

    Defaults.object(string:).

--- Helpers/Defaults.swift
+++ Helpers/Defaults.swift
@@ -13,7 +13,7 @@ class Defaults {
   fileprivate static var defaults = UserDefaults.standard
 
   class func object<T>(for key: Key) -> T? {
-    return object(for: key.rawValue) as? T
+    _obj(for: key.rawValue) as? T
   }
 
   class func set(object: Any?, for key: Key) {
@@ -26,15 +26,19 @@ class Defaults {
   }
 
   class func hasObject(for key: Key) -> Bool {
-    return object(for: key.rawValue) != nil
+    _obj(for: key.rawValue) != nil
   }
 
   class func removeObject(for key: Key) {
     removeObject(for: key.rawValue)
   }
 
-  class func object(for string: String) -> Any? {
-    return defaults.object(forKey: string) as Any?
+  class func _obj(for string: String) -> Any? {
+    defaults.object(forKey: string) as Any?
+  }
+
+  class func object<T>(for string: String) -> T? {
+    defaults.object(forKey: string) as? T
   }
 
   class func set(object: Any?, for string: String) {
